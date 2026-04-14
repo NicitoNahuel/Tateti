7697da8a64b466ddc47ee150cf2f3bfd6a81396a
Nahuel Andre Nicito <131872572+NicitoNahuel@users.noreply.github.com>
2026-04-14 16:17:43 -0300

    Refactor Bot class for improved logic and structure
    Refactor Bot class to improve piece placement and scoring logic.

--- src/Bot.java
+++ src/Bot.java
@@ -1,30 +1,40 @@
 public class Bot{
-    private opc;
-    private rival;
+    private Opciones opc;
+    private Opciones rival;
+    private int[][] tableroPuntuado = new int[3][3];
+    private Random r = new Random();
+
     public void bot(Opciones o){
         this.opc = o;
-        rival = Opciones.contrario(o);
+        this.rival = Opciones.contrario(o);
     }
 
     public int[] colocarPieza(int[][] tablero){  
-        
-    }
-
-    public int[] valorar(int[][] tablero){
+        //como primer movimiento si la casilla del medio esta vacia poner ahi
         if (tablero[1][1] == Opciones.VACIO){
             return {1 , 1}
         }
 
-        new int[][] tableroPuntuado = new int[3][3];
+        valorar() //valorar Tablero
+        return recorrerTablero(-> int[] casillaMax = new int[2];
+                recorrerTablero(-> if (i == 0 && j == 0) {casillaMax = tableroPuntuado[i][j]})
+                recorrerTablero(-> if (tableroPuntuado[i][j] > tableroPuntuado[casillaMax[0]][casillaMax[1]]){
+                    casillaMax[0] = i; casillaMax[1] = j; 
+                })
+                return casillaMax;
+        ) //devuelve donde colocar la pieza
+    }
+
+    public int[] valorar(int[][] tablero){
+        recorrerTablero()//recorrido inicial
 
         for (int i = 0; i<tablero[0].lenght; i++){
             for(int j = 0; j<tablero[1].lenght; j++)
-            {
-                if (tablero[i][j] != Opciones.VACIO){
-                    tableroPuntuado[i][j] = 0;
-                } else {
-                recorrerTablero()
-                }
+            {       
+                if (tableroPuntuado[i][j] != 0){ //valorar individualmente las casillas vacias
+                    //mirar las casillas de al rededor a la casilla vacia
+                    valorarCasilla(i, j);
+                } 
             }
         }
         /*
@@ -35,16 +45,56 @@ public class Bot{
         */
     }
 
-    public Opciones consultarCasilla(int i, int j){
+    private Opciones consultarCasilla(int i, int j){
         return tablero[i][j];
     }
 
-    public void recorrerTablero(Consumer<> funcion){
+    public Opciones getPiezaBot(){
+        return opc;
+    }
+
+    private void ValorarCasilla(int i, int j){
+                     if(tableroPuntuado[i+2][j]){
+
+                    }
+                    if(tableroPuntuado[i+2][j+1])
+                    if(tableroPuntuado[i+2][j-1])
+                    if(tableroPuntuado[i][j+1])
+                    if(tableroPuntuado[i][j-1])
+                    if(tableroPuntuado[i][j])
+                    if(tableroPuntuado[i-1][j])
+                    if(tableroPuntuado[i-1][j-1])
+                    if(tableroPuntuado[i-1][j+1])
+                    
+                    //si fuera la del medio
+                    
+                    if(tableroPuntuado[i+1][j]){
+
+                    }
+                    if(tableroPuntuado[i+1][j+1])
+                    if(tableroPuntuado[i+1][j-1])
+                    if(tableroPuntuado[i][j+1])
+                    if(tableroPuntuado[i][j-1])
+                    if(tableroPuntuado[i][j])
+                    if(tableroPuntuado[i-1][j])
+                    if(tableroPuntuado[i-1][j-1])
+                    if(tableroPuntuado[i-1][j+1])
+    } 
+
+    public void recorrerTablero(Consumer<Opciones o> funcion){
         for (int i = 0; i<tablero[0].lenght; i++){
             for(int j = 0; j<tablero[1].lenght; j++)
             {
-                if()
-                funcion;
+                if(funcion != null)  {
+                    funcion;
+                } else {
+                    //recorrido inicial
+                    if (!consultarCasilla[i][j].equals(Opciones.VACIO)){
+                        tableroPuntuado[i][j] = 0;
+                    } else {
+                        tableroPuntuado[i][j] = 1;
+                    }
+                }
             }
         }
     }
